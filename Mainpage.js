5b28f1ac149442830872c5bf2013559db2ba8d88
Phan Duc Trong <phanductrong.glb@gmail.com>
2019-11-10 15:39:24 +0700

    data base

---
+++ Mainpage.js
@@ -0,0 +1,211 @@
+let month =  [
+    {   
+        time:{
+            date: "1",
+            month: "11",
+            year: "2019"
+        },
+        weather: {
+            morning:{
+                temperate: "30°C",
+                rain: "not",
+                cloudy: "not",
+                sunny: "yes"
+            },
+            daytime :{
+                temperate: "20°C",
+                rain: "yes",
+                cloudy: "not",
+                sunny: "not"
+            },
+            evening :{
+                temperate: "20°C",
+                rain: "not",
+                cloudy: "not",
+                sunny: "yes"
+            }
+        },
+        task = [
+            {
+                category: "to-do",
+                name: "Đọc sách",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 30,
+            },
+            {
+                category: "to-do",
+                name: "Đi làm",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "date",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "routine",
+                name: "gym",
+                noti: "no",
+                noti_moring: "no",
+                noti_hour: 6,
+                noti_min: 30,
+            },
+            {
+                category: "routine",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_hour: "null",
+                noti_min: "null"
+            },
+        ]    
+        
+    },
+    
+    {   
+        time:{
+            date: "2",
+            month: "11",
+            year: "2019"
+        },
+        weather: {
+            morning:{
+                temperate: "10°C",
+                rain: "not",
+                cloudy: "yes",
+                sunny: "not"
+            },
+            daytime :{
+                temperate: "30°C",
+                rain: "no",
+                cloudy: "yes",
+                sunny: "not"
+            },
+            evening :{
+                temperate: "15°C",
+                rain: "not",
+                cloudy: "not",
+                sunny: "yes"
+            }
+        },
+        task = [
+            {
+                category: "deadline",
+                name: "Đọc sách",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 30,
+            },
+            {
+                category: "to-do",
+                name: "Đi làm",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "routine",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "routine",
+                name: "gym",
+                noti: "no",
+                noti_moring: "no",
+                noti_hour: 6,
+                noti_min: 30,
+            },
+            {
+                category: "routine",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_hour: "null",
+                noti_min: "null"
+            },
+        ]    
+        
+    },
+
+    {   
+        time:{
+            date: "2",
+            month: "11",
+            year: "2019"
+        },
+        weather: {
+            morning:{
+                temperate: "10°C",
+                rain: "not",
+                cloudy: "yes",
+                sunny: "not"
+            },
+            daytime :{
+                temperate: "30°C",
+                rain: "no",
+                cloudy: "yes",
+                sunny: "not"
+            },
+            evening :{
+                temperate: "15°C",
+                rain: "not",
+                cloudy: "not",
+                sunny: "yes"
+            }
+        },
+        task = [
+            {
+                category: "deadline",
+                name: "Đọc sách",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 30,
+            },
+            {
+                category: "to-do",
+                name: "Đi làm",
+                noti: "yes",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "routine",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_moring: "yes",
+                noti_hour: 10,
+                noti_min: 35,
+            },
+            {
+                category: "routine",
+                name: "gym",
+                noti: "no",
+                noti_moring: "no",
+                noti_hour: 6,
+                noti_min: 30,
+            },
+            {
+                category: "routine",
+                name: "gặp anh huy",
+                noti: "no",
+                noti_hour: "null",
+                noti_min: "null"
+            },
+        ]    
+        
+    }
+]
