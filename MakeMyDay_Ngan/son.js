53743aa9a6bae6b91eda3af28f965b6b46e56ce4
comsci-hudson <55373864+comsci-hudson@users.noreply.github.com>
2019-11-13 20:31:01 +0700

    Add files via upload

---
+++ MakeMyDay_Ngan/son.js
@@ -0,0 +1,17 @@
+//feedback
+document.getElementById("feedback").addEventListener('click', () => {          
+    document.querySelector('.modal-feedback').style.display = 'flex';
+});
+document.querySelector('.mf-content .close').addEventListener('click', () => {
+    document.querySelector('.modal-feedback').style.display = 'none';
+})
+
+
+//setting
+
+document.getElementById("setting").addEventListener('click', () => {          
+    document.querySelector('.modal-setting').style.display = 'flex';
+});
+document.querySelector('.ms-content .close').addEventListener('click', () => {
+    document.querySelector('.modal-setting').style.display = 'none';
+})
